d642b3dee038a076748ae44257475b6913ef84a8
Carlino Junior <carlinoj@gmail.com>
2021-09-23 19:15:36 -0300

    Carregando aulas anteriores

---
+++ Aula04/DEVER/js/init.js
@@ -0,0 +1,18 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+//CAROUSEL
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
+
+//FormSelects
+
+$(document).ready(function(){
+  $('select').formSelect();
+});
